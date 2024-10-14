3256be576641c46dee60cc9785674981a48acc1a
DJ Jain <dj.jain@mbx.com>
2024-10-14 13:53:03 -0500

    Bump to version 1.1.0
    Breaking change: XmlNode.is_namespace_attribute? has been renamed to
    XmlNode.namespace_attribute?.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SimpleXml.MixProject do
   def project do
     [
       app: :simple_xml,
-      version: "1.0.2",
+      version: "1.1.0",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
