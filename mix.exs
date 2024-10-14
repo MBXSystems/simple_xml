02b19a21a4b85a9357866744ba8183023ca2928a
DJ Jain <dj.jain@mbx.com>
2024-10-14 13:26:26 -0500

    Bump version to v1.0.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SimpleXml.MixProject do
   def project do
     [
       app: :simple_xml,
-      version: "1.0.1",
+      version: "1.0.2",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
